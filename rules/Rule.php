ff079e84c5535499883c8ebc586ed35c28945f6e
Aprod Illes <aprod.illes@gmail.com>
2017-10-07 15:06:38 +0200

    highcard rule

---
+++ rules/Rule.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aprodilles
+ * Date: 2017. 10. 07.
+ * Time: 13:51
+ */
+interface Rule
+{
+
+}
---
+++ tests/HandTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aprodilles
+ * Date: 2017. 10. 07.
+ * Time: 14:18
+ */
+class HandTest
+{
+
+}
