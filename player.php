c794a38f1e276ab4e274762e08138cc7eb976638
Aprod Illes <aprod.illes@gmail.com>
2017-10-07 10:57:12 +0200

    all in fix

--- player.php
+++ player.php
@@ -4,9 +4,9 @@ class Player
 {
     const VERSION = "Default PHP folding player";
 
-    public function betRequest($game_state)
+    public function betRequest($gameState)
     {
-        if (count($game_state["players"]) > 2){
+        if ($this->countActivePlayers($gameState) > 2){
             return 0;
         }
 
@@ -15,5 +15,17 @@ class Player
 
     public function showdown($game_state)
     {
+    }
+
+    public function countActivePlayers($gameState)
+    {
+        $count = 0;
+        foreach ($gameState['players'] as $player){
+            if ($player['status'] == 'active'){
+                $count++;
+            }
+        }
+
+        return $count;
     }
 }
--- tests/PlayerTest.php
+++ tests/PlayerTest.php
@@ -30,10 +30,69 @@ class PlayerTest extends \PHPUnit\Framework\TestCase {
           {
               "id": 1,
               "name": "Chuck",
-              "status": "out",
+              "status": "active",
               "version": "Default random player",
               "stack": 0,
               "bet": 0
+          },
+          {
+              "id": 2,
+              "name": "Bob",
+              "status": "active",
+              "version": "Default random player",
+              "stack": 1590,
+              "bet": 80,
+              "hole_cards": [
+                  {
+                      "rank": "6",
+                      "suit": "hearts"
+                  },
+                  {
+                      "rank": "K",
+                      "suit": "spades"
+                  }
+              ]
+          }
+      ],
+      "community_cards": [
+          {
+              "rank": "4",
+              "suit": "spades"
+          },
+          {
+              "rank": "A",
+              "suit": "hearts"
+          },
+          {
+              "rank": "6",
+              "suit": "clubs"
+          }
+      ]
+  }
+EOL;
+
+    private $gameState2 = <<<EOL
+  {
+      "tournament_id":"550d1d68cd7bd10003000003",
+      "game_id":"550da1cb2d909006e90004b1",
+      "round":0,
+      "bet_index":0,
+      "small_blind": 10,
+      "current_buy_in": 320,
+      "pot": 400,
+      "minimum_raise": 240,
+      "dealer": 1,
+      "orbits": 7,
+      "in_action": 1,
+
+      "players": [
+        {
+              "id": 0,
+              "name": "Albert",
+              "status": "active",
+              "version": "Default random player",
+              "stack": 1010,
+              "bet": 320
           },
           {
               "id": 2,
@@ -93,9 +152,24 @@ EOL;
         $this->assertTrue($response == 0);
     }
 
+    /** @test */
+    public function it_calls_all_in_if_only_2_players_in_the_table()
+    {
+        $player = new \Player();
+
+        $response = $this->betRequest($player, $this->gameState2);
+
+        $this->assertTrue($response == 10000);
+    }
+
     private function betRequest(Player $player, $gameState)
     {
-        $player->betRequest(json_decode($gameState, true));
+        return $player->betRequest(json_decode($gameState, true));
+    }
+
+    private function parse($gameState)
+    {
+        return json_decode($gameState, true);
     }
 
     /** @test */
@@ -103,6 +177,17 @@ EOL;
     {
         $json = json_decode($this->gameState, true);
 
-        $this->assertTrue(is_array());
+        $this->assertTrue(is_array($json));
+    }
+
+    /** @test */
+    public function it_counts_active_players()
+    {
+        $player = new \Player();
+
+        $response = $player->countActivePlayers($this->parse($this->gameState));
+
+        $this->assertEquals(3, $response);
+
     }
 }
